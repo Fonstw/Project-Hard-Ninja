59ae6ee5407c7aeae048121b9f7962b98d98a743
Torben <torbenvdh@gmail.com>
2019-05-09 14:09:54 +0200

    Art Push - Chest update + Animations

---
+++ Assets/Art/Animation/script_ac_Chest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_ac_Chest : MonoBehaviour
+{
+    GameObject obj_Animated;
+    Animator comp_Animator;
+    public bool bool_ChestOpen;
+
+    void Start()
+    {
+        comp_Animator = transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (bool_ChestOpen == true) comp_Animator.SetBool("bool_ChestOpen", true);
+        else comp_Animator.SetBool("bool_ChestOpen", false);
+    }
+}
