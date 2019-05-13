20c9451bb98bc7fdcaa90ed418a0dbcb34f32664
Fonzie! <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2019-05-13 16:57:26 +0200

    Made my tools a little handier for me

--- Assets/Code/DeveloperLaziness.cs
+++ Assets/Code/DeveloperLaziness.cs
@@ -2,18 +2,28 @@
 
 public class DeveloperLaziness : MonoBehaviour
 {
-    [SerializeField] float gameSpeed;
+    [SerializeField] float gameSpeed, gravityForce;
+    Vector3 initialGravity;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        initialGravity = Physics.gravity;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (gameSpeed < 0 || gameSpeed > 100)
+            gameSpeed = Mathf.Clamp(gameSpeed, 0, 100);
+
         if (Time.timeScale != gameSpeed)
             Time.timeScale = gameSpeed;
+
+        if (gravityForce < -9999999 || gravityForce > 9999999)
+            gravityForce = Mathf.Clamp(gravityForce, -9999999, 9999999);
+
+        if (Physics.gravity != initialGravity * gravityForce)
+            Physics.gravity = initialGravity * gravityForce;
     }
 }
--- Assets/Code/Meshurments.cs
+++ Assets/Code/Meshurments.cs
@@ -2,17 +2,43 @@
 
 public class Meshurments : MonoBehaviour
 {
-    [SerializeField] float roundTo;
-    
+    [SerializeField] bool log = true;
+    [SerializeField] int roundTo = -1;
+    Vector3 sizes = Vector3.zero;
+
     void Start()
     {
-        Vector3 sizes = Vector3.zero;
-
-        if (GetComponent<Mesh>() != null) sizes = GetComponent<Mesh>().bounds.size;
-        else if (GetComponent<MeshCollider>() != null) sizes = GetComponent<MeshCollider>().bounds.size;
+        if (GetComponent<MeshCollider>() != null) sizes = GetComponent<MeshCollider>().bounds.size;
         else if (GetComponent<MeshRenderer>() != null) sizes = GetComponent<MeshRenderer>().bounds.size;
         else if (GetComponent<Collider>() != null) sizes = GetComponent<Collider>().bounds.size;
 
-        print(name + "'s measured sizes are " + sizes.x.ToString("F" + roundTo) + " by " + sizes.y.ToString("F" + roundTo) + " by " + sizes.z.ToString("F" + roundTo));
+        if (log)
+        {
+            if (roundTo >= 0)
+                print(name + "'s measured sizes are " + sizes.x.ToString("F" + roundTo) + " by " + sizes.y.ToString("F" + roundTo) + " by " + sizes.z.ToString("F" + roundTo));
+            else
+                print(name + "'s measured sizes are " + sizes.x + " by " + sizes.y + " by " + sizes.z);
+        }
+    }
+
+    public Vector3 GetSizes()
+    {
+    	return sizes;
+    }
+
+    public Vector3 GetRoundedSizes()
+    {
+        if (roundTo >= 0)
+        {
+            Vector3 answer = Vector3.zero;
+
+            answer.x = float.Parse(sizes.x.ToString("F" + roundTo));
+            answer.y = float.Parse(sizes.y.ToString("F" + roundTo));
+            answer.z = float.Parse(sizes.z.ToString("F" + roundTo));
+
+            return answer;
+        }
+        else
+            return sizes;
     }
 }
