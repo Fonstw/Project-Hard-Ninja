6902e49985fcaec0c513fb4be3fa61e00fc6bd33
Fonzie! <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2019-05-13 13:15:11 +0200

    Added easy mesh measurement script
    Useful for the level design

---
+++ Assets/Code/Meshurments.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Meshurments : MonoBehaviour
+{
+    [SerializeField] float roundTo;
+    
+    void Start()
+    {
+        Vector3 sizes = Vector3.zero;
+
+        if (GetComponent<Mesh>() != null) sizes = GetComponent<Mesh>().bounds.size;
+        else if (GetComponent<MeshCollider>() != null) sizes = GetComponent<MeshCollider>().bounds.size;
+        else if (GetComponent<MeshRenderer>() != null) sizes = GetComponent<MeshRenderer>().bounds.size;
+        else if (GetComponent<Collider>() != null) sizes = GetComponent<Collider>().bounds.size;
+
+        print(name + "'s measured sizes are " + sizes.x.ToString("F" + roundTo) + " by " + sizes.y.ToString("F" + roundTo) + " by " + sizes.z.ToString("F" + roundTo));
+    }
+}
