e40b4130010c1f4c9e763c3e22cb52fff6aac1a7
Fonzie! <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2019-05-13 11:38:01 +0200

    BGM implemented
    Also dev's laziness tool made

---
+++ Assets/Code/DeveloperLaziness.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DeveloperLaziness : MonoBehaviour
+{
+    [SerializeField] float gameSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeScale != gameSpeed)
+            Time.timeScale = gameSpeed;
+    }
+}
---
+++ Assets/Code/NoDuplicateBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class NoDuplicateBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameObject.FindGameObjectsWithTag(gameObject.tag).Length == 1)   // if there is no-one like me...
+            DontDestroyOnLoad(gameObject);   // make sure I don't ever go away!
+        else   // else there is someone like me...
+            Destroy(gameObject);   // so I shouldn't interfere, time to kill myself bye!
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
