68a3ebbdb7e4c3d072345d161b4e21665a5b302c
Robbert <BayanganNaga@hotmail.com>
2019-05-22 11:28:28 +0200

    Ball Monster prefab + script changes

--- Assets/Code/EnemyBehaviour.cs
+++ Assets/Code/EnemyBehaviour.cs
@@ -5,6 +5,7 @@ public class EnemyBehaviour : MonoBehaviour
     [SerializeField] float walkSpeed = 5;
     [SerializeField] float runSpeed = 8;
     [SerializeField] Transform myVisionLight;
+    [SerializeField] Transform monsterMesh;
     [SerializeField] float groundDistance = 2f;
     float speed;
     bool right = true;
@@ -57,6 +58,7 @@ public class EnemyBehaviour : MonoBehaviour
     {
         right = !right;
         myVisionLight.Rotate(0, 180, 0, Space.World);
+        monsterMesh.Rotate(0,180,0, Space.World);
     }
 
     private void OnTriggerStay(Collider other)
