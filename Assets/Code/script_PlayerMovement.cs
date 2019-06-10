475b5500b96f3fb0d02d7da020b07ab6b80a21a3
Torben <torbenvdh@gmail.com>
2019-06-10 13:38:25 +0200

    Auto stash before merge of "master" and "origin/master"

--- Assets/Code/script_PlayerMovement.cs
+++ Assets/Code/script_PlayerMovement.cs
@@ -8,13 +8,13 @@ public class script_PlayerMovement : MonoBehaviour
     //public bool b_EnableGravity = false;
     //public float f_Gravity = -9.81f;
 
-    //movement variables
+    //Movement variables
     public float f_DoubleTapTime = .5f;
     public float f_MovementSpeed = 5f;
     public bool b_SidewaysCollision;
     Vector3 v3_MovementDirection;
 
-    //jumping variables
+    //Jumping variables
     public float f_JumpForce;
     public float f_GroundedDistance = 1f;
     public bool b_Jumping;
@@ -24,13 +24,13 @@ public class script_PlayerMovement : MonoBehaviour
     Rigidbody comp_Rigidbody;
     Collider comp_BoxCollider;
 
-    Animator anim;
+    //Animation variables
+    public GameObject obj_AnimatorHolder;
+    int intSwitch_Animation;
+    List<float> fList_LastMoveInputValue = new List<float>();
 
-
-    private void Awake()
-    {
-        anim = GetComponent<Animator>();
-    }
+    //References
+    Animator comp_Animator;
 
     void Start()
     {
@@ -38,17 +38,18 @@ public class script_PlayerMovement : MonoBehaviour
 
         comp_Rigidbody = GetComponent<Rigidbody>();
         comp_BoxCollider = GetComponent<BoxCollider>();
+        comp_Animator = obj_AnimatorHolder.GetComponent<Animator>();
     }
 
     void Update()
     {
         function_GetInputs();
-        function_Jump();
-        function_Movement();
     }
 
     void FixedUpdate()
     {
+        function_Jump();
+        function_Movement();
     }
 
     bool bool_CheckIfGrounded()
@@ -61,24 +62,18 @@ public class script_PlayerMovement : MonoBehaviour
         //input for movement
         v3_MovementDirection = new Vector3(Input.GetAxis("Move"), 0);
 
-
-
         //input for jumping
-        if (Input.GetButtonDown("Jump") && bool_CheckIfGrounded())
- 
-            b_Jumping = true;
-        
-            
-
-            else if (Input.GetButtonUp("Jump"))
-            b_Jumping = false;
+        if (Input.GetButtonDown("Jump") && bool_CheckIfGrounded()) b_Jumping = true;
+        else if (Input.GetButtonUp("Jump")) b_Jumping = false;
     }
 
     // prevent player mistakes (eg. don't stop jumping if let go of button very short time)
     void function_Jump()
     {
         if (b_Jumping)
-            if(f_JumpTimer == f_BaseJumpTimer)
+        {
+            function_PlayAnimation("Jump");
+            if (f_JumpTimer == f_BaseJumpTimer)
             {
                 comp_Rigidbody.velocity = new Vector3(comp_Rigidbody.velocity.x, 0);
                 comp_Rigidbody.AddForce(Vector3.up * f_JumpForce);
@@ -86,24 +81,106 @@ public class script_PlayerMovement : MonoBehaviour
             }
             else if (f_JumpTimer > 0)
             {
-                comp_Rigidbody.AddForce(Vector3.up * f_JumpForce * f_JumpTimer * .05f);
+                comp_Rigidbody.AddForce(Vector3.up * f_JumpForce * f_JumpTimer * .1f);
                 f_JumpTimer -= Time.deltaTime;
             }
             else
             {
                 b_Jumping = false;
             }
+        }
         else
             f_JumpTimer = f_BaseJumpTimer;
     }
 
     void function_Movement()
     {
+        //Animation
+        if (bool_CheckIfGrounded())
+        {
+            bool b_Moved = false;
+            if(fList_LastMoveInputValue != null)
+                for (int i = 0; i < fList_LastMoveInputValue.Count; i++)
+                {
+                    if (fList_LastMoveInputValue[i] != 0) b_Moved = true;
+                }
+
+            if (Input.GetAxis("Move") != 0 || b_Moved || v3_MovementDirection != Vector3.zero)
+            {
+                function_PlayAnimation("Run");
+            }
+            else
+            {
+                function_PlayAnimation("Idle");
+            }
+        }
+        else if (!b_Jumping)
+        {
+            if (Input.GetAxis("Move") != 0)
+            {
+                function_PlayAnimation("Run");
+            }
+            else
+            {
+                function_PlayAnimation("Idle");
+            }
+        }
+
+        //flip player model to direction
+        if (Input.GetAxisRaw("Move") != 0)
+        {
+            if (Input.GetAxisRaw("Move") == -1) transform.localScale = new Vector3(-1, 1, 1);
+            else if (Input.GetAxisRaw("Move") == 1) transform.localScale = new Vector3(1, 1, 1);
+        }
+
         if (!Physics.SphereCast(transform.position, .15f, v3_MovementDirection,out RaycastHit ray_hitInfo, .01f))
         {
             comp_Rigidbody.velocity = new Vector3(v3_MovementDirection.x * f_MovementSpeed, comp_Rigidbody.velocity.y);
-
-
         }
+
+        fList_LastMoveInputValue.Add(Input.GetAxisRaw("Move"));
+        if(fList_LastMoveInputValue.Count > 5)
+            fList_LastMoveInputValue.RemoveAt(0);
+    }
+
+    void function_PlayAnimation(string string_AnimationToPlay)
+    {
+        if (string_AnimationToPlay == "Idle") intSwitch_Animation = 1;
+        if (string_AnimationToPlay == "Jump") intSwitch_Animation = 2;
+        if (string_AnimationToPlay == "Attack") intSwitch_Animation = 3;
+        if (string_AnimationToPlay == "Run") intSwitch_Animation = 4;
+
+        switch (intSwitch_Animation)
+        {
+            case 1:
+                //Idle
+                comp_Animator.SetBool("IsDrunk", true);
+                comp_Animator.SetBool("IsJumping", false);
+                comp_Animator.SetBool("IsAttacking", false);
+                comp_Animator.SetBool("IsRunning", false);
+                return;
+            case 2:
+                //Jump
+                comp_Animator.SetBool("IsDrunk", false);
+                comp_Animator.SetBool("IsJumping", true);
+                comp_Animator.SetBool("IsAttacking", false);
+                comp_Animator.SetBool("IsRunning", false);
+                return;
+            case 3:
+                //Attack
+                comp_Animator.SetBool("IsDrunk", false);
+                comp_Animator.SetBool("IsJumping", false);
+                comp_Animator.SetBool("IsAttacking", true);
+                comp_Animator.SetBool("IsRunning", false);
+                return;
+            case 4:
+                //Run
+                comp_Animator.SetBool("IsDrunk", false);
+                comp_Animator.SetBool("IsJumping", false);
+                comp_Animator.SetBool("IsAttacking", false);
+                comp_Animator.SetBool("IsRunning", true);
+                return;
+        }
+
     }
 }
